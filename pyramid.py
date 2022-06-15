cae0e6dcb4a964e269c88721be0ee0aaed320d47
thisguyaaron <azelenski123@gmail.com>
2022-06-15 14:39:34 -0700

    alomst done with part 2. finished pyramid and timer and call counter

--- pyramid.py
+++ pyramid.py
@@ -1,9 +1,14 @@
 import sys
 import time
+from time import perf_counter
 
+pyramid_depth = 7
+call_counter = 0
 
 def weight_on(r, c):
     weight_const = 200
+    global call_counter
+    call_counter += 1
 
     # case 1 top of tree
     if r == 0 and c == 0:
@@ -25,9 +30,22 @@ def weight_on(r, c):
         return weight
 
 
-def main():
-    pass
+def print_weight_pyramid():
+    global pyramid_depth
+    start_timer = time.perf_counter()
+    for i in range(0, pyramid_depth):
+        print("")
+        for j in range(0, i + 1):
+            print(f" %.2f" % (weight_on(i, j)), end=" ")
+    end_timer = time.perf_counter()
+    elapsed_time = end_timer - start_timer
+    print()
+    print()
+    print(f"Elapsed time: {elapsed_time} seconds")
+    print(f"Function calls: {call_counter}")
 
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) != 1:
+        PYRAMID_DEPTH = int(sys.argv[1])
+
